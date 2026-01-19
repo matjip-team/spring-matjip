feef45182d88104d623c229e8e6efd2de2fdb8c3
xxx0209 <makelotus0209@gmail.com>
2026-01-19 11:06:19 +0900

    first commit

---
+++ src/main/java/com/restaurant/matjip/MatjipApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant.matjip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatjipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatjipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restaurant/matjip/MatjipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant.matjip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatjipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
