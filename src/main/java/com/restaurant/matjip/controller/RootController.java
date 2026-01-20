ee2fc750eb9aa1ce4ac23bfbcb08f1a21c8a3263
xxx0209 <makelotus0209@gmail.com>
2026-01-20 14:45:49 +0900

    자바접속 포트 8080으로변경

--- src/main/java/com/restaurant/matjip/controller/RootController.java
+++ src/main/java/com/restaurant/matjip/controller/RootController.java
@@ -1,9 +1,9 @@
 package com.restaurant.matjip.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class RootController {
 
     @GetMapping("/")
