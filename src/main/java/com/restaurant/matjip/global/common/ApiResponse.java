fba1a20a14f8f12dfcae99a1d146696d02a289bc
xxx0209 <makelotus0209@gmail.com>
2026-02-03 16:25:45 +0900

    마이페이지 일부 수정

--- src/main/java/com/restaurant/matjip/global/common/ApiResponse.java
+++ src/main/java/com/restaurant/matjip/global/common/ApiResponse.java
@@ -7,18 +7,31 @@ import lombok.Getter;
 @AllArgsConstructor
 public class ApiResponse<T> {
     private boolean success;
+    private String message;
     private T data;
     private ErrorResponse error;
 
     public static <T> ApiResponse<T> success(T data) {
-        return new ApiResponse<>(true, data, null);
+        return new ApiResponse<>(true, null, data, null);
+    }
+
+    public static <T> ApiResponse<T> success(String message, T data) {
+        return new ApiResponse<>(true, message, data, null);
+    }
+
+    public static <T> ApiResponse<T> success(String message) {
+        return new ApiResponse<>(true, message, null, null);
+    }
+
+    public static <T> ApiResponse<T> success() {
+        return new ApiResponse<>(true, null, null, null);
     }
 
     public static <T> ApiResponse<T> fail(String code, String message) {
-        return new ApiResponse<>(false, null, new ErrorResponse(code, message, null));
+        return new ApiResponse<>(false, null,null, new ErrorResponse(code, message, null));
     }
 
     public static <T> ApiResponse<T> fail(String code, String message, Object fields) {
-        return new ApiResponse<>(false, null, new ErrorResponse(code, message, fields));
+        return new ApiResponse<>(false, null, null, new ErrorResponse(code, message, fields));
     }
 }
